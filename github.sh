62c45eae1717876a132fd82f2aeb75cfaa0ecd6c
Wan <yuxi.wan@gmail.com>
2017-09-30 20:40:45 +0800

    add ssh steps

--- github.sh
+++ github.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
 # for GitHub on linux
+
+
+# Step1: Generate public/private ssh key
+
+cd ~/.ssh
+ssh-keygen
+cat ~/.ssh/id_rsa.pub
+
+# copy public key to github.com setting
+
+# Step 2
+
 git clone https://github.com/wanyx2015/cloudadmin.git
 git remote rm origin
 git remote add origin git@github.com:wanyx2015/cloudadmin.git
@@ -11,5 +23,7 @@ git config --global user.email "yuxi.wan@gmail.com"
 
 # will output Hi wanyx2015! You've successfully authenticated, but GitHub does not provide shell access.
 ssh -T git@github.com
-# git push -u origin master
+git push -u origin master
+
+
 
